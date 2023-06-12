11d681f99b7dc572a261b3779e5be743090b0867
Silvio <deviseartegraficolr@gmail.com>
2023-06-12 10:25:43 -0300

    Este codigo calcula el indice de masa corporal.

--- clase5/AdivinaNumero.java
+++ clase5/AdivinaNumero.java
@@ -1,4 +1,8 @@
-package clase5;
+/*5) Juego de adivinar el número:
+        Genera un número aleatorio entre 1 y 100. Pídele al usuario que adivine el número.*/
 
 public class AdivinaNumero {
+    public static void main (String arg[]){
+
+    }
 }

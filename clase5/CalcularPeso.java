c04ac2bbb015b1678f6951182033cd16bfb39ddb
Silvio <deviseartegraficolr@gmail.com>
2023-06-12 08:59:06 -0300

    Este codigo calcula el indice de masa corporal.

--- clase5/CalcularPeso.java
+++ clase5/CalcularPeso.java
@@ -1,4 +1,36 @@
-package clase5;
+import java.util.Scanner;
 
-public class CalcularPeso {
+public class Main {
+    public static void main(String arg[]) {
+        Scanner cargarDatos = new Scanner(System.in);
+        //Peso en KG / altura en metros al cuadrado
+
+        System.out.println("ingresa tu peso en kilogramos");
+        double peso = cargarDatos.nextDouble();
+        System.out.println("ingresa tu altura metros");
+        double altura = cargarDatos.nextDouble();
+        double indiceMasaCorporal = peso / (altura * altura);
+        System.out.println("Tu indice de masa corporal es " + indiceMasaCorporal);
+
+
+
+        if (indiceMasaCorporal >=30) {
+            System.out.println("Estas en estado de obesidad");
+        }
+
+        // Entre 25 y 30 Sobrepeso
+        if (indiceMasaCorporal >= 25 && indiceMasaCorporal <29.9 ) {
+            System.out.println("tienes sobrepeso");
+        }
+        // Pesao Normal 18.5 a 24.9
+        if (indiceMasaCorporal >= 18.5 && indiceMasaCorporal <24.9) {
+            System.out.println("Tu peso es normal");
+        }
+
+        //Menor a 18.6 debajo del normal
+        if (indiceMasaCorporal < 18.6) {
+            System.out.println("Estas por debajo del peso normal");
+
+        }
+    }
 }

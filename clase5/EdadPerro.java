376bd52621db52d01f99ecb50cfa74dcf4108087
Silvio <deviseartegraficolr@gmail.com>
2023-06-12 09:34:48 -0300

    Este codigo calcula el indice de masa corporal.

--- clase5/EdadPerro.java
+++ clase5/EdadPerro.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 
 public class EdadPerro {
     public static void main(String arg[]) {
-        Scanner cargarDatos = new Scanner(System.in);
+        Scanner ingresaEdad = new Scanner(System.in);
 
+        /*1) Calculadora de edad de perros:
+        Pídele al usuario que ingrese la edad de su perro.
+        Convierte esa edad a años de perro (se dice que 1 año
+        humano equivale a 7 años de perro).
+         */
+        System.out.println("Ingresa la edad en años humanos de tu perro:");
+
+        Integer edad = ingresaEdad.nextInt();
+
+        Integer edadPerro = edad * 7;
+
+        System.out.println("La edad real de tu perro es de: " + edadPerro + " Años");
 
     }
 }

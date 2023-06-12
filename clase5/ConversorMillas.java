7bf49a69b1c77267972ac7f066067cd7fbfbd3f9
Silvio <deviseartegraficolr@gmail.com>
2023-06-12 09:50:35 -0300

    Este codigo calcula el indice de masa corporal.

---
+++ clase5/ConversorMillas.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+public class ConversorMillas {
+
+    public static void main(String arg[]) {
+        /*2) Conversor de millas a kilómetros:
+        Pídele al usuario que ingrese una distancia en millas.
+        Convierte la distancia a kilómetros utilizando la fórmula: kilómetros = millas * 1.60934.
+        Muestra la distancia en kilómetros al usuario.*/
+
+        Scanner CragaMillas = new Scanner(System.in);
+
+        System.out.print("Ingresa una distancia en millas: ");
+
+        Double Millas = CragaMillas.nextDouble();
+
+        Double Resultado = Millas* 1.60934;
+
+        System.out.print(" El resultado convertido a kilometros es de: " + Resultado + " Kilometros");
+
+    }
+
+    }

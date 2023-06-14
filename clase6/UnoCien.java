0f755fda10688ce2b32eecc3929fcba19cceb07c
Silvio <deviseartegraficolr@gmail.com>
2023-06-14 00:37:39 -0300

    Ejercicion de la clase Nº6

---
+++ clase6/UnoCien.java
@@ -0,0 +1,15 @@
+
+
+
+package clase6;
+
+public class UnoCien {
+    public static void maim (String arg[]){
+        int numero = 1;
+
+        while (numero <=100){
+            numero ++;
+            System.out.print(numero);
+        }
+    }
+}
